183bbf1027659958578878013fde3138722cd261
Abhay0809 <abhayanandkk23@gmail.com>
2022-09-25 20:48:26 +0530

    Final Project

---
+++ layout/_footer.php
@@ -0,0 +1,8 @@
+<footer class="foter">
+<b>Made By: <a href="https://abhay-web-folio.netlify.app/">Abhay Anand</a></b>
+</footer>
+
+
+
+</body>
+</html>
---
+++ users/layout/_footer.php
@@ -0,0 +1,8 @@
+<footer class="nav navbar-fixed-bottom">
+            <b>Made By: <a href="https://abhay-web-folio.netlify.app/">Abhay Anand</a></b>
+</footer>
+
+
+
+</body>
+</html>
